559c2bbe2790a686721341a4edf1485ec8462877
louiszawadzki <louisz@theodo.fr>
2017-10-12 19:23:11 +0200

    Add comments

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const lg = console.log;
 
-const styledComponents = (...args) => args;
+const styledComponents = (...args) => {
+  const strings = args.shift(); // Store array of strings in variable
+  return strings.reduce((result, element, index, array) => {
+    result.push(element); // Add the next element
+    (index < array.length - 1) && result.push(args[index]); // Add the next interpolated value
+    return result;
+  }, []).join(''); // Join everything to get the final string
+};
 
 const padding = '6px';
 lg(styledComponents`padding: ${padding}`);
